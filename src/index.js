f1a17668b5826207ce764d8a6e87cad2c924339c
mihaelapas <mihaelapas@ext.inditex.com>
2020-04-06 16:50:48 +0200

    Add redux base structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+import {applyMiddleware, createStore} from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers"
+import thunk from "redux-thunk";
 
 ReactDOM.render(
-<App/>,
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+        <App/>
+    </Provider>,
     document.querySelector("#root")
 );
