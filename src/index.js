525ca187834491926f5802d46c073ccd425648b6
mihaelapas <mihaelapas@ext.inditex.com>
2020-04-07 11:36:58 +0200

    Add translations and an example for how to do it

--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import {applyMiddleware, createStore} from "redux";
 import App from "./components/App";
 import reducers from "./reducers"
 import thunk from "redux-thunk";
+import {IntlProvider} from "react-intl";
+
+// import messages_es from "./translations/es.json"; // use this to change to spanish
+import messages_en from "./translations/en.json";
 
 ReactDOM.render(
     <Provider store={createStore(reducers, applyMiddleware(thunk))}>
-        <App/>
+        <IntlProvider locale="en" messages={messages_en}>
+            <App />
+        </IntlProvider>
     </Provider>,
     document.querySelector("#root")
 );
