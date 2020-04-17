88d2c75776c3d1161eaa9e71e2c9d09f0c503503
mihaelapas <mihaelapas@ext.inditex.com>
2020-04-17 10:08:19 +0200

    Move the footer and header to have it available for all pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,28 @@ import Home from './Home'
 import history from '../history'
 
 import { Router, Switch, Route } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import BrandToolBar from './BrandToolBar'
+import Footer from './Footer'
 
 const App = () => {
   return (
     <div>
       <div className="ui container">
-        <Router history={history}>
-          <div>
-            <Switch>
-              <Route path="/" exact component={Home}/>
-            </Switch>
-          </div>
-        </Router>
+        <React.Fragment>
+          <CssBaseline/>
+          <BrandToolBar/>
+          <main>
+            <Router history={history}>
+              <div>
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                </Switch>
+              </div>
+            </Router>
+          </main>
+          <Footer/>
+        </React.Fragment>
       </div>
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,16 @@
 import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Footer from './Footer'
 import Header from './Header'
 import CategoryList from './categories/CategoryList'
-import BrandToolBar from './BrandToolBar'
 import NeedUpdate from './NeedUpdate'
 
 const Home = () => {
   return (
     <React.Fragment>
-      <CssBaseline/>
-      <BrandToolBar/>
-      <main>
-        <Header/>
-        <NeedUpdate/>
-        <CategoryList/>
-      </main>
-      <Footer/>
+      <Header/>
+      <NeedUpdate/>
+      <CategoryList/>
     </React.Fragment>
+
   )
 }
 
