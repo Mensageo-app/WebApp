beee115d6a5cf333867a2af387bee5c94384813f
Jonathan Fernandez <jfernandez@MacBook-Pro-de-Jonathan.local>
2020-04-23 15:12:39 +0200

    Link to home page added to the header (BrandToolBar component)

--- src/components/BrandToolBar.js
+++ src/components/BrandToolBar.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { FormattedMessage } from 'react-intl'
+import Link from '@material-ui/core/Link'
 
 const BrandToolBar = () => {
   return <Toolbar>
     <Typography variant="h5">
-      <FormattedMessage id="app.text" defaultMessage="Mensageo"/>
+      <Link href="/" color="inherit">
+        <FormattedMessage id="app.text" defaultMessage="Mensageo" />
+      </Link>
     </Typography>
   </Toolbar>
 }
