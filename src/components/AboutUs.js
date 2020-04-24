368bb58d7f830004da170e03e3ed59bec24f6ad0
Jonathan Fernandez <jfernandez@MacBook-Pro-de-Jonathan.local>
2020-04-24 15:52:00 +0200

    adding content to about us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,29 +1,48 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { useStyles } from '../style/HeaderStyle'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import { FormattedMessage } from 'react-intl'
+import Container from '@material-ui/core/Container'
+
+const AboutUsContent = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" align="left">
+            <FormattedMessage id={props.title} defaultMessage="Missing String" />
+          </Typography>
+          <div>
+            <p>
+              <FormattedMessage id={props.content} defaultMessage="Missing String" />
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
 
 const AboutUs = () => {
-  return <Typography variant="h5">
-        Our Vision
+  const classes = useStyles()
+  return <div className={classes.heroContent}>
+    <Container>
+      <div className={classes.heroTitle}>
+        <Typography variant="h3" align="left" gutterBottom>
+          <FormattedMessage id="aboutus.aboutus" defaultMessage="Missing String for aboutus.aboutus" />
+        </Typography>
+      </div>
+      <div className={classes.heroButtons}>
 
-        Mensageo is a platform for hospitals and elderly centers,
-        who have a shortage of medical supplies and don&apos;t have a
-        reliable way to communicate these needs to the people who
-        can provide them. Our platform will connect individuals and
-        organizations that want to make donations with the hospitals
-        and elderly centers in need.
+        <AboutUsContent title="aboutus.vision.title" content="aboutus.vision.content" />
+        <AboutUsContent title="aboutus.whoarewe.title" content="aboutus.whoarewe.content" />
+        <AboutUsContent title="aboutus.contactus.title" content="aboutus.contactus.content" />
 
-        Who are we?
-
-        We are a group of people with diverse backgrounds seeking to contribute
-        with hospitals and Elderly centers around the world to get the protective
-        personal equipment they need.
-
-        Contact us!
-
-        If you have any questions or suggestions please send us an email to xxx@mensageo.com
-        and we will get back to you as soon as possible. Thanks!
-
-  </Typography>
+      </div>
+    </Container>
+  </div>
 }
 
 export default AboutUs
