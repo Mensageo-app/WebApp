3c0b26fe821b73f943e8604a1a1ccc312e8a3a01
Jonathan Fernandez <jfernandez@MacBook-Pro-de-Jonathan.local>
2020-04-25 19:38:43 +0200

    spacing boxes in about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
-import { useStyles } from '../style/HeaderStyle'
+import { useStyles } from '../style/AboutUsStyle'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import { FormattedMessage } from 'react-intl'
 import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
 
 const AboutUsContent = (props) => {
   return (
     <div>
-      <Card>
-        <CardContent>
-          <Typography variant="h6" align="left">
-            <FormattedMessage id={props.title} defaultMessage="Missing String" />
-          </Typography>
-          <div>
-            <p>
-              <FormattedMessage id={props.content} defaultMessage="Missing String" />
-            </p>
-          </div>
-        </CardContent>
-      </Card>
+      <Box m={2}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6" align="left">
+              <FormattedMessage id={props.title} defaultMessage="Missing String" />
+            </Typography>
+            <div>
+              <p>
+                <FormattedMessage id={props.content} defaultMessage="Missing String" />
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </Box>
     </div>
   )
 }
---
+++ src/style/AboutUsStyle.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles((theme) => ({
+  heroContent: {
+    background: theme.palette.backgroundMain,
+    padding: theme.spacing(2, 0, 6),
+    color: '#ffffff',
+    mixBlendMode: 'normal',
+    boxShadow: theme.palette.boxShadow
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+    color: '#ffffff'
+  },
+  heroTitle: {
+    width: '70%'
+  }
+}))
