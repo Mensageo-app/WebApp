dab689f06eb8bdf27ad219769bf45b9cbfd74b7e
meg-deb <magdalena.debowicz@thoughtworks.com>
2020-04-28 15:28:50 +0200

    Rearanged fields in the form

--- src/components/makers/MakersForm.js
+++ src/components/makers/MakersForm.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import Button from '@material-ui/core/Button';
-import { TextField, FormControl } from '@material-ui/core';
+import Button from '@material-ui/core/Button'
+import { Paper, Grid, TextField } from '@material-ui/core'
 
-const MakersForm = () => 
-    <>
-        <TextField required id="FullName" label="Full Name" variant="outlined" />
-        <TextField required id="Telephone" label="Telephone" variant="outlined" />
-        <TextField required id="Email" label="Email" variant="outlined" />
-        <TextField required id="Amount" label="Amount" variant="outlined" />
-        <TextField required id="DonationInfo" label="Information about the donation" variant="outlined" multiline/>
-        <TextField id="Company" label="Company" variant="outlined" />
-        <TextField id="AdditionalTelephone" label="Additional telephone" variant="outlined" />
-        <TextField id="AdditionalEmail" label="Additional email" variant="outlined" />
-        <Button variant="contained">Submit</Button>
-    </>
+const MakersForm = () =>
+  <>
+    <form>
+      <Paper style={{ padding: 45 }} elevation={0} >
+        <Grid container alignItems="flex-start" spacing={2}>
+          <Grid item xs={6}>
+            <TextField fullWidth required id ="FullName" type="text" label="Full Name" />
+
+            <TextField fullWidth required id ="Telephone" type="text" label="Telephone" />
+
+            <TextField fullWidth required id ="Email" type="text" label="Email" />
+
+            <TextField fullWidth required id ="Amount" type="text" label="Amount" />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField fullWidth id ="Company" type="text" label="Company" />
+
+            <TextField fullWidth id ="AdditionalTelephone" type="text" label="Additional telephone" />
+
+            <TextField fullWidth id ="AdditionalEmail" type="text" label="Additional email" />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField fullWidth required id ="DonationInfo" type="text" label="Information about the donation" multiline rowsMax={5}/>
+          </Grid>
+          <Grid item xs={12} align="right" style={{ paddingTop: 20, paddingRight: 80 }}>
+            <Button variant="contained" color="primary" type="submit">Submit</Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </form>
+  </>
 
 export default MakersForm
--- src/components/makers/MakersForm.test.js
+++ src/components/makers/MakersForm.test.js
@@ -2,34 +2,27 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import MakersForm from './MakersForm'
 
-describe("<MakersForm />", () => {
+describe('<MakersForm />', () => {
+  describe('Structural tests', () => {
+    it('Renders component', () => {
+      const { getByText } = render(<MakersForm></MakersForm>)
 
-    describe("Structural tests", () => {
-
-        it( "Renders component", () => {
-
-            const {getByText} = render(<MakersForm></MakersForm>)
-
-            expect(getByText("Submit")).toBeTruthy()
-
-        })
-
-        it.each`
-            label    
-            ${"Full Name *"}
-            ${"Telephone *"}
-            ${"Email *"}
-            ${"Amount *"}
-            ${"Information about the donation *"}
-            ${"Company"}
-            ${"Additional telephone"}
-            ${"Additional email"}
-             `('should have "$label" entry', ({label}) => {
-            const {getByLabelText} = render(<MakersForm></MakersForm>)
-            expect(getByLabelText(label)).toBeTruthy()
-        });
-
+      expect(getByText('Submit')).toBeTruthy()
     })
 
+    it.each`
+            label    
+            ${'Full Name *'}
+            ${'Telephone *'}
+            ${'Email *'}
+            ${'Amount *'}
+            ${'Information about the donation *'}
+            ${'Company'}
+            ${'Additional telephone'}
+            ${'Additional email'}
+             `('should have "$label" entry', ({ label }) => {
+      const { getByLabelText } = render(<MakersForm></MakersForm>)
+      expect(getByLabelText(label)).toBeTruthy()
+    })
+  })
 })
-
