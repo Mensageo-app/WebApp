8e5ab9b02202e5cedf0892e4e67130e6293832e9
jcardenal <jcarde@thoughtworks.com>
2020-04-30 16:13:16 +0200

    Added heading message to makers form dialog

--- src/components/makers/MakersForm.js
+++ src/components/makers/MakersForm.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import { Paper, Grid, TextField, DialogTitle, Dialog, DialogContent, DialogContentText } from '@material-ui/core'
 
-const MakersForm = ({ open, onClose }) =>
+const MakersForm = ({ open, onClose, hospitalName, product }) =>
   <Dialog open={open} onClose={onClose}>
     <DialogTitle></DialogTitle>
     <DialogContent>
-      <DialogContentText></DialogContentText>
+      <DialogContentText>
+        Thank you for contacting hospital {hospitalName} in order to donate {product}
+
+        In order for the hospital to be able to evaluate if your donation can be accepted or not,
+        we kindly request you to include all the relevant information to this end, including if you
+        hold any kind of certification or if you are using a model that has already
+        been certified
+      </DialogContentText>
       <form>
         <Paper style={{ padding: 45 }} elevation={0} >
           <Grid container alignItems="flex-start" spacing={2}>
