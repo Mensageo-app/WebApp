bd18e582c01d8854048cb13bd5bd4e6245f58ac4
Tsvetan Nenov <tsvetan.nenov@thoughtworks.com>
2020-05-04 10:30:26 +0200

    Add redux-logger to the project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux'
+import { createLogger } from 'redux-logger'
 
 import App from './components/App'
 import reducers from './reducers'
@@ -14,8 +15,10 @@ import Theme from './style/Theme'
 // import messagesEs from "./translations/es.json"; // use this to change to spanish
 import messagesEn from './translations/en.json'
 
+const logger = createLogger();
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+  <Provider store={createStore(reducers, applyMiddleware(thunk, logger))}>
     <MuiThemeProvider theme={Theme}>
       <IntlProvider locale="en" messages={messagesEn}>
         <App/>
