21a4ee5b332228428815ab586a38fda3788054c1
Tsvetan Nenov <tsvetan.nenov@thoughtworks.com>
2020-05-04 15:07:09 +0200

    Fix wrong import in the store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import api from '../apis/backend'
-import { FETCH_CATEGORIES, FETCH_CATEGORY, FET, FETCH_HOSPITAL_NEEDS } from './types'
+import { FETCH_CATEGORIES, FETCH_CATEGORY, FETCH_HOSPITAL_NEEDS } from './types'
 
 export const fetchCategories = () => async dispatch => {
   const res = await api.get('/categories')
