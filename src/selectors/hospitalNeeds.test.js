47f93b774e9d94666954c5f7512cbc84632f614a
Ana Jimenez <ana.jimenezvalbuena@thoughtworks.com>
2020-06-03 13:22:06 +0200

    Fixed makersForm test

--- src/selectors/hospitalNeeds.test.js
+++ src/selectors/hospitalNeeds.test.js
@@ -1,4 +1,4 @@
-import { calculateProductQuantity, calculateProductQuantityPerHospital } from './hospitalNeeds'
+import { calculateProductQuantity } from './hospitalNeeds'
 
 describe('HospitalNeeds selector', () => {
   describe('calculateProductQuantity', () => {
@@ -7,11 +7,11 @@ describe('HospitalNeeds selector', () => {
         id: 1,
         hospital: {
           id: 2,
-          name: "Hospital Central"
+          name: 'Hospital Central'
         },
-        product:  {
+        product: {
           id: 1,
-          name: "Masks"
+          name: 'Masks'
         },
         quantity: 5,
         request_timestamp: '2020-05-24 17:00:00'
@@ -20,12 +20,12 @@ describe('HospitalNeeds selector', () => {
         id: 2,
         hospital: {
           id: 1,
-          name: "The hospital"
+          name: 'The hospital'
         },
         product: {
           id: 1,
-          name: "Masks"
-        } ,
+          name: 'Masks'
+        },
         quantity: 4,
         request_timestamp: '2020-05-24 17:00:00'
       },
@@ -33,11 +33,11 @@ describe('HospitalNeeds selector', () => {
         id: 3,
         hospital: {
           id: 4,
-          name: "Hospital4"
+          name: 'Hospital4'
         },
         product: {
           id: 2,
-          name: "tests"
+          name: 'tests'
         },
         quantity: 40,
         request_timestamp: '2020-05-24 17:00:00'
@@ -46,11 +46,11 @@ describe('HospitalNeeds selector', () => {
         id: 3,
         hospital: {
           id: 2,
-          name: "Hospital Central"
+          name: 'Hospital Central'
         },
         product: {
           id: 2,
-          name: "tests"
+          name: 'tests'
         },
         quantity: 17,
         request_timestamp: '2020-05-24 17:00:00'
@@ -72,36 +72,36 @@ describe('HospitalNeeds selector', () => {
   })
 
   describe('calculateProductQuantityPerHospital', () => {
-    const hospitalNeeds = [
-      {
-        id: 1,
-        hospital_id: 1,
-        product_id: 1,
-        quantity: 3,
-        request_timestamp: '2020-05-24 17:00:00'
-      },
-      {
-        id: 2,
-        hospital_id: 2,
-        product_id: 1,
-        quantity: 4,
-        request_timestamp: '2020-05-24 17:00:00'
-      },
-      {
-        id: 3,
-        hospital_id: 2,
-        product_id: 1,
-        quantity: 6,
-        request_timestamp: '2020-05-24 17:00:00'
-      },
-      {
-        id: 3,
-        hospital_id: 2,
-        product_id: 2,
-        quantity: 17,
-        request_timestamp: '2020-05-24 17:00:00'
-      }
-    ]
+    // const hospitalNeeds = [
+    //   {
+    //     id: 1,
+    //     hospital_id: 1,
+    //     product_id: 1,
+    //     quantity: 3,
+    //     request_timestamp: '2020-05-24 17:00:00'
+    //   },
+    //   {
+    //     id: 2,
+    //     hospital_id: 2,
+    //     product_id: 1,
+    //     quantity: 4,
+    //     request_timestamp: '2020-05-24 17:00:00'
+    //   },
+    //   {
+    //     id: 3,
+    //     hospital_id: 2,
+    //     product_id: 1,
+    //     quantity: 6,
+    //     request_timestamp: '2020-05-24 17:00:00'
+    //   },
+    //   {
+    //     id: 3,
+    //     hospital_id: 2,
+    //     product_id: 2,
+    //     quantity: 17,
+    //     request_timestamp: '2020-05-24 17:00:00'
+    //   }
+    // ]
 
     // it('Returns product quantity for a given hospital', () => {
     //   expect(calculateProductQuantityPerHospital(1, 1, hospitalNeeds)).toBe(3)
