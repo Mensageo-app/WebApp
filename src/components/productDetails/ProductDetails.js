cd731a7f523017bf8755c415e63f2c95358cac91
Reynau <xavi.lopez.reynau@gmail.com>
2020-06-08 16:39:44 +0200

    Fix ProductDetails layout

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -2,27 +2,31 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import { useStyles } from '../../style/ProductStyle'
-import CardMedia from '@material-ui/core/CardMedia'
 import Box from '@material-ui/core/Box'
+import { Container } from '@material-ui/core'
 
 const ProductDetails = ({ product }) => {
   const classes = useStyles()
 
   return (
-    <Box component="p" m={6} >
+    <Container maxWidth="lg" className={classes.root}>
       <Box m="1rem">
         <Typography variant="h4" align="left" >{product.name}</Typography>
       </Box>
-
-      <Grid container spacing={2} justify="center" >
-        <Grid item md={3}>
-          <CardMedia className={classes.cardMedia} image={'/images/mask.jpeg'} position={'left'}/>
+      <Grid container spacing={2}>
+        <Grid item>
+          <div className={classes.image}>
+            <img className={classes.img} alt="complex" src='/images/mask.jpeg' />
+          </div>
         </Grid>
-        <Grid item md={9}>
+        <Grid item sm container>
+          <Grid item>
           <Typography variant="body1" component="p" position={'right'}>{product.description}</Typography>
+          </Grid>
         </Grid>
       </Grid>
-    </Box>
+    </Container>
+    
   )
 }
 
--- src/style/ProductStyle.js
+++ src/style/ProductStyle.js
@@ -1,6 +1,28 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
+
+  root: {
+    flexGrow: 1,
+    marginTop: '30px',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 256,
+    height: 256,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+
+
   cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(4)
